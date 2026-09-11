0d553a4acc748015f787cb91bf36634ccad4d1b0
Mikha Januardi Millenn Simanjuntak <mikha.simanjuntak@xsyscemerlang.com>
2026-09-11 19:08:57 +0700

    fix: exclude static files from middleware (webmanifest, ico, png)

--- src/middleware.ts
+++ src/middleware.ts
@@ -70,8 +70,8 @@ export const config = {
      * - _next/static (static files)
      * - _next/image (image optimization files)
      * - favicon.ico (favicon file)
-     * - public folder
+     * - public folder (static files like .webmanifest, .ico, .png)
      */
-    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.png$).*)",
+    "/((?!api|_next/static|_next/image|favicon.ico|.*\\.webmanifest$|.*\\.ico$|.*\\.png$).*)",
   ],
 };
